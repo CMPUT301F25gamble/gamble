24fade68f45717b9c4ad1d548ecae84ffe7c24c6
GaurangBhana <bhana@ualberta.ca>
2025-09-23 18:40:01 -0600

    Added color to the hexagon

--- app/src/main/java/com/example/gamble/Hexagon.java
+++ app/src/main/java/com/example/gamble/Hexagon.java
@@ -4,6 +4,7 @@ public class Hexagon extends Shape{
     private int sides;
     private float apothem;
     private float perimeter;
+    private String color = "yellow";
 
     public Hexagon(){
         sides = 6;
